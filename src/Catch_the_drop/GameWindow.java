9dd3b1194f537eaba80e136eeb14df0a2d7f6fc2
den2dd <dd.rutikov@gmail.com>
2017-01-04 18:54:24 +0300

    Начало работы над движением капель. Одна капля отрисована в окошке.

--- src/Catch_the_drop/GameWindow.java
+++ src/Catch_the_drop/GameWindow.java
@@ -7,27 +7,30 @@ import java.io.IOException;
 
 public class GameWindow extends JFrame{
 
-    private static GameWindow game_Window;
+    private static GameWindow game_window;
     private static Image background;
     private static Image game_over;
     private static Image drop;
+    private static float drop_left = 200;
+    private static float drop_top = 200;
+
     public static void main(String[] args) throws IOException {
         background = ImageIO.read(GameWindow.class.getResourceAsStream("background.png"));
         game_over = ImageIO.read(GameWindow.class.getResourceAsStream("game_over.png"));
         drop = ImageIO.read(GameWindow.class.getResourceAsStream("drop.png"));
-        game_Window = new GameWindow();
-        game_Window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        game_Window.setLocation(200, 100);
-        game_Window.setSize(906, 478);
-        game_Window.setResizable(false);
+        game_window = new GameWindow();
+        game_window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        game_window.setLocation(200, 100);
+        game_window.setSize(906, 478);
+        game_window.setResizable(false);
         GameField game_field = new GameField();
-        game_Window.add(game_field);
-        game_Window.setVisible(true);
+        game_window.add(game_field);
+        game_window.setVisible(true);
     }
 
     private static void onRepaint(Graphics g){
         g.drawImage(background, 0, 0, null);
-
+        g.drawImage(drop, (int) drop_left, (int) drop_top, null);
     }
     private static class GameField extends JPanel{
         @Override
