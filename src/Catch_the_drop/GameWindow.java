c39231f59cdcadf07c69ae6d1434cbe8a0613002
den2dd <dd.rutikov@gmail.com>
2017-01-04 18:46:51 +0300

    Созданы три переменных для фона, капли и надписи "Game Over", и загружены в них соответствующие картинки. Создан фон окошка для будущей игры.

--- src/Catch_the_drop/GameWindow.java
+++ src/Catch_the_drop/GameWindow.java
@@ -1,12 +1,20 @@
 package Catch_the_drop;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class GameWindow extends JFrame{
 
     private static GameWindow game_Window;
-    public static void main(String[] args) {
+    private static Image background;
+    private static Image game_over;
+    private static Image drop;
+    public static void main(String[] args) throws IOException {
+        background = ImageIO.read(GameWindow.class.getResourceAsStream("background.png"));
+        game_over = ImageIO.read(GameWindow.class.getResourceAsStream("game_over.png"));
+        drop = ImageIO.read(GameWindow.class.getResourceAsStream("drop.png"));
         game_Window = new GameWindow();
         game_Window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         game_Window.setLocation(200, 100);
@@ -18,6 +26,7 @@ public class GameWindow extends JFrame{
     }
 
     private static void onRepaint(Graphics g){
+        g.drawImage(background, 0, 0, null);
 
     }
     private static class GameField extends JPanel{
