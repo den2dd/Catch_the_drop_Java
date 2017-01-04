a664e8c69b9d54f260e16456b5879b4d4902a24f
den2dd <dd.rutikov@gmail.com>
2017-01-04 18:35:10 +0300

    Создание окошка 906х478 для будущей игры.

--- src/Catch_the_drop/GameWindow.java
+++ src/Catch_the_drop/GameWindow.java
@@ -1,8 +1,30 @@
 package Catch_the_drop;
 
-public class GameWindow {
+import javax.swing.*;
+import java.awt.*;
 
+public class GameWindow extends JFrame{
+
+    private static GameWindow game_Window;
     public static void main(String[] args) {
-	// write your code here
+        game_Window = new GameWindow();
+        game_Window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        game_Window.setLocation(200, 100);
+        game_Window.setSize(906, 478);
+        game_Window.setResizable(false);
+        GameField game_field = new GameField();
+        game_Window.add(game_field);
+        game_Window.setVisible(true);
+    }
+
+    private static void onRepaint(Graphics g){
+
+    }
+    private static class GameField extends JPanel{
+        @Override
+        protected void paintComponent(Graphics g){
+            super.paintComponent(g);
+            onRepaint(g);
+        }
     }
 }
